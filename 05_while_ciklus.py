bc1e8a7dd509e92d28135071068d38598ab14651
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-10-14 08:39:03 +0200

    add

--- 05_while_ciklus.py
+++ 05_while_ciklus.py
@@ -29,3 +29,8 @@ szam = 10
 while szam <= 1:
     print(szam)
     szam -= 1
+
+"""5. Feladat
+Írj egy programot, amely a felhasználótól páros számot kér be. Amennyiben a megadott szám páratlan, újra és újra megtörténik az adatbekérés mindaddig,
+amíg végül páros számot nem ad meg a felhasználó.
+"""

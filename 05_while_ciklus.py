2801928d387ab2514137ef708fb2482c46a3b1af
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-10-14 09:30:52 +0200

    valtoztatasok

--- 05_while_ciklus.py
+++ 05_while_ciklus.py
@@ -39,7 +39,8 @@
 #     numero -= 2
 
 """5. Feladat
-Írj egy programot, amely a felhasználótól páros számot kér be. Amennyiben a megadott szám páratlan, újra és újra megtörténik az adatbekérés mindaddig,
+Írj egy programot, amely a felhasználótól páros számot kér be.
+Amennyiben a megadott szám páratlan, újra és újra megtörténik az adatbekérés mindaddig,
 amíg végül páros számot nem ad meg a felhasználó.
 """
 
